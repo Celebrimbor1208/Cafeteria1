8fd68311870ff8945a221f87fc7d9fc079b11de1
Celebrimbor1208 <114787923+Celebrimbor1208@users.noreply.github.com>
2026-06-29 17:33:06 -0500

    Matthew
    GOOOOOOOOOOOOO

--- Mi-Cafeteria-CoffeeFast-main/frontend/src/config/axios.ts
+++ Mi-Cafeteria-CoffeeFast-main/frontend/src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://cafeteria1-20vd.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
